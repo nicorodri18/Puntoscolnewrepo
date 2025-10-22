553dd673f55ef27b1c52a8cd2e1d4683099e3c5e
nicorodri18 <100327214+nicorodri18@users.noreply.github.com>
2025-10-22 10:26:07 -0500

    Arreglo con migracion local completa sin supabase

--- supabaseClient.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// 🔗 Supabase Project Credentials
-const SUPABASE_URL = 'https://xfhmqxgbrmpijmwcsgkn.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhmaG1xeGdicm1waWptd2NzZ2tuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA0Mzk5MTQsImV4cCI6MjA3NjAxNTkxNH0.s3OW5OSp2t7XpNg6WV2ygjXTQhWzMQBjqviHj7y0qzk';
-
-// ⚙️ Initialize Supabase client
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
